39a186b212527cbd5e79cc63ee1ce426a68e349e
meli-diteixeira <dihogo.teixeira@mercadolivre.com>
2021-09-07 23:28:36 -0300

    dockerize backend

--- src/main/java/com/devops/dimdimbackend/controller/AgenciaController.java
+++ src/main/java/com/devops/dimdimbackend/controller/AgenciaController.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.devops.dimdimbackend.model.Agencia;
 import com.devops.dimdimbackend.repository.AgenciaRepository;
 
-@CrossOrigin(origins = "http://localhost:8081")
+//@CrossOrigin(origins = "http://dimdim-frontend:8081")
 @RestController
 @RequestMapping("/api")
 public class AgenciaController {
